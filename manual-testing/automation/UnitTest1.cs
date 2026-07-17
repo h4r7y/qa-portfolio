d4677c578b753c3c4a5073b56a2b11b73fecc3d8
Harald Sorg <harald.sorg@outlook.de>
2026-07-17 13:52:43 +0000

    Clear Commit

---
+++ manual-testing/automation/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace automation;
+
+using Microsoft.Playwright; 
+using NUnit.Framework;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+
+    public class LoginTest
+    {
+        [Test]
+        public async Task TestLogin()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(
+            new BrowserTypeLaunchOptions { Headless = true });
+        
+            var page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://www.saucedemo.com");
+      
+            await page.FillAsync("#user-name", "standard_user");
+            await page.FillAsync("#password", "secret_sauce");
+            await page.ClickAsync("#loginButton");
+        
+            Assert.That(page.Url, Does.Contain("inventory"));
+        }
+
+    }
+}
+

e879c1a0dbed36701ce38d4b6b893bafb4fb863e
Harald Sorg <harald.sorg@outlook.de>
2026-07-17 15:20:23 +0000

    Add LoginTest

---
+++ manual-testing/automation/LoginTest.cs
@@ -0,0 +1,20 @@
+﻿namespace automation;
+
+using Microsoft.Playwright.NUnit; 
+using NUnit.Framework;
+
+public class LoginTest : PageTest
+{
+    [Test]
+    public async Task TestLogin()
+    {
+        await Page.GotoAsync("https://www.saucedemo.com");
+        await Page.FillAsync("#user-name", "standard_user");
+        await Page.FillAsync("#password", "secret_sauce");
+        await Page.ClickAsync("#login-button");
+
+        Assert.That(Page.Url, Does.Contain("inventory"));
+    }
+}
+
+
